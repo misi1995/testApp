2b972a4e536961f98c336bdec3bf396dbc5e12ef
shijianfeng <1158934589@qq.com>
2020-06-15 10:58:19 +0800

    111

--- utils/screen.js
+++ utils/screen.js
@@ -3,7 +3,7 @@
  * [日期] 2018-04-01
  * [作者] sun
  */
-import { Dimensions, PixelRatio } from 'react-native';
+import {Dimensions, PixelRatio} from 'react-native';
 
 export default {
   width: Dimensions.get('window').width, // iphone6 375
